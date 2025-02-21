0b06d3745310a7da82e880d5241611f6adca721e
gunavardhan <gunavardhan@ibm.com>
2025-02-21 14:12:16 +0530

    Final Check

--- publish_release.js
+++ publish_release.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 let nextReleaseTag, latestReleaseTag
 
 if(latestReleaseTag === undefined){
-    nextReleaseTag = "r10001"
+    nextReleaseTag = "r10002"
 } else{
     let releaseNumber = /^r([0-9]+)$/g.exec(latestReleaseTag.name)[1]
     nextReleaseTag = "r" + (parseInt(releaseNumber)+1)
