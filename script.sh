1ce0bd740d0b633d313ed04a0471dc4008295d9c
gunavardhan <gunavardhan@ibm.com>
2025-02-27 11:37:36 +0530

    cron Resolved

--- script.sh
+++ script.sh
@@ -46,15 +46,15 @@ cat env.yaml
 
 echo "Checking for changes, adding them, committing and pushing..."
 git status
-git add env.yaml
+git add env.yaml env.yaml.bak
 git commit -m "New commit with release number $rNumber"
 git push origin "$rNumber"
 
 echo "Existing PR'S: "
-gh pr lists --author "@gvram13541" --label "spec_version"
+gh pr lists--author "@gvram13541" --label "spec_version"
 
 echo "Creating a pull request..."
-gh pr create --repo gvram13541/DEMO --base main --head "$rNumber" --title "New PR with $rNumber" --body "Automated PR from Travis for release $rNumber"
+# gh pr create --repo gvram13541/DEMO --base main --head "$rNumber" --title "New PR with $rNumber" --body "Automated PR from Travis for release $rNumber"
 
 echo "New pr List: "
 gh pr list
