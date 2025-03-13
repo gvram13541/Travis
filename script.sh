85a38d38d93339b410d29a30d4d288b7c7b4c14f
gunavardhan <gunavardhan@ibm.com>
2025-03-13 10:16:15 +0530

    ic

--- script.sh
+++ script.sh
@@ -53,7 +53,7 @@ if [ -n "$pr_list" ]; then
     git pull --rebase origin "$branch"
 
     echo "Modifying the env.yaml file..."
-    sed -i.bak "s/api_spec_version: */api_spec_version: $rNumber/g" env.yaml
+    sed -i.bak "s/api_spec_version: [a-zA-Z0-9]*/api_spec_version: $rNumber/g" env.yaml
     
     echo "Content of env.yaml after modification:"
     cat env.yaml
